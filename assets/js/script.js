8028ed96f5326ade61b2c25b6cddd4def9305689
m-a-h-e <maik.heese@web.de>
2019-10-06 21:05:00 +0200

    changed to small default image style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,28 @@
 
 $(document).ready(function() {
-  $("img").each(function(){
-    $(this).addClass("small");
+  $("img").each(function() {
     if ($(this).parent().is("p")) {
-      $(this).parent().addClass("small");
-      $(this).parent().addClass("caption");
       $(this).parent().before('<div class="clearfix"></div>');
+      $(this).parent().wrap('<div class="img"></div>');
+      $(this).insertBefore($(this).parent());
+    } else {
+      $(this).before('<div class="clearfix"></div>');
+      $(this).wrap('<div class="img"></div>');
     }
   });
-  $("img").on('click', function(){
-    $(this).toggleClass("small");
-    if ($(this).parent().is("p")) {
-      $(this).parent().toggleClass("small");
+  $("div.img").on('click', function() {
+    $(this).toggleClass("large-img");
+  });
+
+  $("ul").each(function() {
+    if (!$(this).parent().is("li")) {
+      if ($(this).has("li").has("ul").length) {
+        $(this).addClass("top-ul");
+        $(this).children("li").each(function() {
+          //console.log($(this).text());
+          // TODO wrap top-ul li text into <span>...</span> but without other (ul/li) sub-elements
+        });
+      }
     }
   });
 });
